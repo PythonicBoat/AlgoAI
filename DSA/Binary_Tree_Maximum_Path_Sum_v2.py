cb8883e0daeda9ba41c4fa8a85dfbcd2ab7bc770
Ekjot Singh <workforekjot@gmail.com>
2022-10-05 19:11:13 +0530

    Created one more solution og Binary Tree as v2

---
+++ DSA/Binary_Tree_Maximum_Path_Sum_v2.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxPathSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.res = float('-inf')
+        self.helper(root)
+        return self.res 
+        
+    def helper(self, root):
+        if not root:
+            return 0
+        left, right = self.helper(root.left), self.helper(root.right)
+        self.res = max(self.res, root.val + left + right)
+        return max(root.val + max(left, right), 0)
